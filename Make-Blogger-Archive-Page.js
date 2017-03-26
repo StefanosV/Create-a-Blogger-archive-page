5bc2ebd8549a68c3e94721fb1310dfd30e48841c
Stefanos Vamvakos <svamvakos@hotmail.com>
2017-03-26 18:15:37 +0300

    Update Make-Blogger-Archive-Page.js

--- Make-Blogger-Archive-Page.js
+++ Make-Blogger-Archive-Page.js
@@ -1,37 +1,34 @@
-function LoadTheArchive(TotalFeed) 
-{
-    var PostTitles = new Array();
-    var PostURLs = new Array();
-    var PostYears = new Array();
-    var PostMonths = new Array();
-    var PostDays = new Array();
-    if("entry" in TotalFeed.feed) 
-    {
-	var PostEntries=TotalFeed.feed.entry.length;
-	for(var PostNum=0; PostNum<PostEntries ; PostNum++) 
-	{
-	    var ThisPost = TotalFeed.feed.entry[PostNum];
-	    PostTitles.push(ThisPost.title.$t);
-	    PostYears.push(ThisPost.published.$t.substring(0,4));
-	    PostMonths.push(ThisPost.published.$t.substring(5,7));
-	    PostDays.push(ThisPost.published.$t.substring(8,10));
-	    var ThisPostURL;
-	    for(var LinkNum=0; LinkNum < ThisPost.link.length; LinkNum++) 
-	    {
-		if(ThisPost.link[LinkNum].rel == "alternate") 
-		{
-		    ThisPostURL = ThisPost.link[LinkNum].href;
-		    break
-		}
-	    }
-	    PostURLs.push(ThisPostURL);
+function LoadTheArchive(TotalFeed) {
+  var PostTitles = new Array();
+  var PostURLs = new Array();
+  var PostYears = new Array();
+  var PostMonths = new Array();
+  var PostDays = new Array();
+  if("entry" in TotalFeed.feed) 
+  {
+    var PostEntries=TotalFeed.feed.entry.length;
+    for(var PostNum=0; PostNum<PostEntries ; PostNum++) 
+      {
+        var ThisPost = TotalFeed.feed.entry[PostNum];
+	PostTitles.push(ThisPost.title.$t);
+	PostYears.push(ThisPost.published.$t.substring(0,4));
+	PostMonths.push(ThisPost.published.$t.substring(5,7));
+	PostDays.push(ThisPost.published.$t.substring(8,10));
+	var ThisPostURL;
+	for(var LinkNum=0; LinkNum < ThisPost.link.length; LinkNum++) {
+          if(ThisPost.link[LinkNum].rel == "alternate") {
+	    ThisPostURL = ThisPost.link[LinkNum].href;
+            break;
+	  }
 	}
+	PostURLs.push(ThisPostURL);
+      }
     }
-    DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays);
+  DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays);
 }
 
 function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays) {
-  var MonthNames=["Ιανουάριος","Φεβρουάριος","Μάρτιος","Απρίλιος","Μάιος","Ιούνιος","Ιούλιος","Αύγουστος","Σεπτέμβριος","Οκτώβριος","Νοέμβριος","Δεκέμβριος"];
+  var MonthNames=["Ιανουαρίου","Φεβρουαρίου","Μαρτίου","Απριλίου","Μαΐου","Ιουνίου","Ιουλίου","Αυγούστου","Σεπτεμβρίου","Οκτωβρίου","Νοεμβρίου","Δεκεμβρίου"];
   var NumberOfEntries=PostTitles.length;
   for(var EntryNum = 0; EntryNum < NumberOfEntries; EntryNum++) {
     NameOfMonth = MonthNames[parseInt(PostMonths[EntryNum],10)-1];
@@ -44,6 +41,5 @@ function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays) {
     else if (PostYears[EntryNum] == "2015") {
       document.getElementById("fifteen").innerHTML += ('<br/><a href ="' + PostURLs[EntryNum] + '">' + PostTitles[EntryNum] + "</a> (" + parseInt(PostDays[EntryNum],10) + " " + NameOfMonth + " " + PostYears[EntryNum] + ")");
     }
-    //document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+"</a> ("+NameOfMonth+" "+parseInt(PostDays[EntryNum],10)+", "+PostYears[EntryNum]+")<br />");
   }
 }
