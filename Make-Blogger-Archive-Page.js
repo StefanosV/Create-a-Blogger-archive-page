e87a936cea9c1eb9a564792f5b92d6deb3a94b8a
Stefanos Vamvakos <svamvakos@hotmail.com>
2017-03-26 17:29:02 +0300

    Update Make-Blogger-Archive-Page.js

--- Make-Blogger-Archive-Page.js
+++ Make-Blogger-Archive-Page.js
@@ -38,7 +38,7 @@ function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays)
     {
 	NameOfMonth = MonthNames[parseInt(PostMonths[EntryNum],10)-1];
 	if (PostYears[EntryNum] == "2017") {
-	  document.innerHTML += ('<a href ="' + PostURLs[EntryNum] + '">' + PostTitles[EntryNum] + "</a> (" + parseInt(PostDays[EntryNum],10) + " " + NameOfMonth + " " + PostYears[EntryNum] + ")<br />");
+	  document.write('<a href ="' + PostURLs[EntryNum] + '">' + PostTitles[EntryNum] + "</a> (" + parseInt(PostDays[EntryNum],10) + " " + NameOfMonth + " " + PostYears[EntryNum] + ")<br />");
 	  //document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+"</a> ("+NameOfMonth+" "+parseInt(PostDays[EntryNum],10)+", "+PostYears[EntryNum]+")<br />");
 	}
     }
